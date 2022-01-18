c39102d98f12d03ab279228b80508a4c90ae6381
Pavan Reddy <52646611+pavan-reddy8@users.noreply.github.com>
2022-01-18 13:49:21 -0500

    Time: 54 ms (12.05%), Space: 14 MB (99.38%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -7,25 +7,25 @@ class Solution:
         res=[]
 
         while(l<r and t<b):
-            print(res)
+     
             for p in range(l,r):
                 res.append(m[t][p])
             t +=1
-            print(res,1)
+       
             for q in range(t,b):
                 res.append(m[q][r-1])  
             r -=1
-            print(res,2)
+       
             if not (l<r and t<b):
                 break
             for p in range(r-1,l-1,-1):
                 res.append(m[b-1][p])
             b-=1
-            print(res,3)
+      
             for q in range(b-1,t-1,-1):
                 res.append(m[q][l])
             l+=1
-            print(res,4)
+          
         return res
             
             
