5ad5bbaf45bfd34ec936ecbcbd9cf61ab29705ca
Pavan Reddy <52646611+pavan-reddy8@users.noreply.github.com>
2022-06-30 13:52:01 -0400

    Time: 1106 ms (42.98%), Space: 27.5 MB (15.14%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -7,7 +7,7 @@ class Solution:
         while l < r:
             
             area = (r-l) * min(h[l],h[r])
-            maxarea = area if maxarea < area else maxarea
+            maxarea = max(maxarea,area)
             if h[l] < h[r]:
                 l+=1
             elif h[l] > h[r]:
