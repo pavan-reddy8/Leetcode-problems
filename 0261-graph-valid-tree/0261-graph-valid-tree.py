c842168f50239ee4d8598038be3f086ebecad668
Pavan Reddy <52646611+pavan-reddy8@users.noreply.github.com>
2023-02-18 04:33:47 +0530

    Time: 119 ms (22.09%), Space: 17.6 MB (7.51%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -0,0 +1,29 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        d = defaultdict(list)
+        for (v1,v2) in edges:
+            d[v1].append(v2)
+            d[v2].append(v1)
+        
+        visited = set()
+        cycle = set()
+        print(d)
+        def dfs(v,pv):
+            # print(cycle)
+            if v in cycle:
+                return False
+            visited.add(v)
+            cycle.add(v)
+            res = True
+            for vi in d[v]:
+                if vi != pv:
+                    res &= dfs(vi,v)
+            return res
+        
+        
+        res = dfs(0,-1)
+        if len(cycle) != n:
+            return False
+        return res
+                
+        
