33b3e6a7e2a307395c512829cb7d1ad9433ad491
Pavan Reddy <52646611+pavan-reddy8@users.noreply.github.com>
2023-03-23 20:13:42 -0400

    Time: 27 ms (96.69%), Space: 13.9 MB (66.17%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -3,23 +3,23 @@ class Solution:
         d = defaultdict(int)
         for si in s:
             d[si] += 1
-        print(d)
+
         heap = []
         q =[]
         res = ''
         for di in d:
             heap.append([-d[di],di])
-        print(heap)
+
         heapq.heapify(heap)
         prev = None
         
         while heap or prev:
-            print(heap,prev)
+
             if prev and not heap:
-                print(res)
+
                 return ''
             cur = heapq.heappop(heap)
-            print(cur)
+
             cur[0] += 1
             res += cur[1]
             
