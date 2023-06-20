3517fc26d9367d4bf24da7a06a0dc2162516bdcd
Pavan Reddy <52646611+pavan-reddy8@users.noreply.github.com>
2023-06-20 17:19:15 -0400

    Time: 60 ms (5.95%), Space: 16.3 MB (69.46%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        numscopy = copy.deepcopy(nums)
+        for i in range(len(numscopy)):
+            if numscopy[i] == val:
+                nums.remove(val)
+                
+            else:
+                k+=1
+            nums.sort()
+        return k
+        
